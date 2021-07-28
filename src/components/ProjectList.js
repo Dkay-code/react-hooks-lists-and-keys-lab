0918d8f74b2339ab76315f72f2ab217d8cdebe3d
DKay-code <derya.kaygisiz@outlook.com>
2021-07-28 15:27:27 +0300

    Done.

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,9 +6,35 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */
+      projects.map((project)=>{
+      return <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies}/>
+      })
+      }</div>
     </div>
   );
 }
 
 export default ProjectList;
+
+
+// // ColorItem component
+// function ColorItem(props) {
+//   return <li style={{ color: props.color }}>{props.color}</li>;
+// }
+
+// // ColorList component
+// function ColorList() {
+//   const colors = [
+//     "firebrick",
+//     "rebeccapurple",
+//     "salmon",
+//     "darkslategray",
+//     "hotpink",
+//   ];
+
+//   const colorElements = colors.map((color) => {
+//     return <ColorItem key={color} color={color} />;
+//   });
+//   // etc
+// } 
